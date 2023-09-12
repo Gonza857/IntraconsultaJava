31a4696d2791653da985babdcef95ac4d6010d49
Gonzalo Ramos <gonzaloalexramos@gmail.com>
2023-09-12 13:16:34 -0300

    actualizar variable 2

--- PB2-Clase-30-8/src/ar/unlam/pb2/Universidad.java
+++ PB2-Clase-30-8/src/ar/unlam/pb2/Universidad.java
@@ -14,7 +14,7 @@ public class Universidad {
 	}
 
 	public void calificar(Integer dniAlumno, Integer codMateria, Integer nota) {
-		String pruebaGit = "nueva caja";
+		String pruebaGit = "nueva jujuujuj";
 		Alumno alumnoBuscado = buscarAlumno(dniAlumno);
 		if (alumnoBuscado != null) {
 			Materia buscarMateria = alumnoBuscado.buscarMateriaDeAlumno(codMateria);
